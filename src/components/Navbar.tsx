c4c57d841534b7b73afd3c866186ac372f623154
Igor <iluzarraga@gmail.com>
2023-08-23 01:58:12 +0200

    Changes made in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link'
 import MenuSmallScreen from './MenuSmallScreen'
+import CartIcon from './CartIcon'
+import Image from 'next/image'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+    const user = false
+
     return (
         <div className='h-12 text-purple-500 p-4 flex items-center justify-between 
                         border-b-2 border-b-purple-500 uppercase md:h-24 lg:px-20 xl:px-40'
         >
+            {/* LEFT LINKS */}
+            <div className='hidden md:flex gap-4 flex-1'>
+                <Link href="/">Homepage</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/">Contact</Link>
+            </div>
+
             {/* LOGO */}
-            <div className='text-xl'>
+            <div className='text-xl md:font-bold flex-1 md:text-center'>
                 <Link
                     href="/"
                 >
@@ -21,7 +32,24 @@ const Navbar = (props: Props) => {
             <div className='md:hidden'>
                 <MenuSmallScreen />
             </div>
-        </div>
+
+            {/* RIGTH LINKS */}
+            <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+                <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer
+                              bg-green-300 px-1 rounded-md"
+                >
+                    <Image src="/phone.png" alt="Phone icon" width={20} height={20} />
+                    <span>123 456 78</span>
+                </div>
+
+                {!user ? (
+                    <Link href="/login">Login</Link>
+                ) : (
+                    <Link href="/orders">Orders</Link>
+                )}
+                <CartIcon />
+            </div>
+        </div >
     )
 }
 
